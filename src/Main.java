645928ffa2d99cf77da2f405fe0afaef92e35b6f
AlexandrZinoviev <z.alexu@mail.ru>
2022-08-18 22:48:32 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> intList = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
+        Collections.sort(intList);
+        for (int i = 0; i < intList.size(); i++) {
+            int x = intList.get(i);
+            if (x > 0)
+                if (x % 2 == 0)
+                    System.out.println(x + " ");
+        }
+    }
+}
